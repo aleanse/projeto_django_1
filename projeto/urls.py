b7a37ed393293c7a5a020bb293cb66879f354d23
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2023-10-12 01:12:02 -0300

    mensagem

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,21 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-def home(request):
-    return HttpResponse('home')
 
-def sobre(request):
-    return HttpResponse('sobre')
 
-def contato(request):
-    return HttpResponse('contato')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
+    path('',include('recipes.urls'))
+
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    return render(request,'recipes/home.html')
+
+def sobre(request):
+    return HttpResponse('sobre 1')
+
+def contato(request):
+    return HttpResponse('contato')
