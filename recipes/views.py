8c86b442c0490aa3ddc88be623a1658d30082625
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2023-10-18 21:54:45 -0300

    aa

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 # Create your views here.
 def home(request):
-    return render(request,'pages/home.html')
+    return render(request,'pages/home.html',context={'recipes':[make_recipe() for _ in range (10)]})
+
+
+def recipe(request, id):
+    return render(request,'pages/recipe-view.html',context={'recipes':[make_recipe()] ,'is_detail_page':True,})
 
 
